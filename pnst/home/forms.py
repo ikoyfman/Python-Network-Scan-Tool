79da88f4fbb37d095e4caa1e1138ca0eea4368d9
Ilya Koy <Ilya.koy@outlook.com>
2019-07-12 07:20:27 -0400

    changed validation on forms.py so subnet can except nothing

--- pnst/home/forms.py
+++ pnst/home/forms.py
@@ -10,6 +10,8 @@ class ScanForm(forms.ModelForm):
     
     def clean_subnet(self):
         subnet = self.cleaned_data['subnet']
-        if subnet > 32 or subnet < 1:
+        if not subnet:
+            return  subnet
+        elif subnet > 32 or subnet < 1:
             raise forms.ValidationError("The subnet can only be between 1 and 32")
-        return subnet
+
